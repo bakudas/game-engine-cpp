5c74e452808d63ee7e66344b10c6c3268b2e7ed7
bakudas <bakudas@gmail.com>
2020-02-08 18:36:00 -0300

    Initial Commit

---
+++ GameEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngine
+//
+//  Created by Wilson Melo on 08/02/20.
+//  Copyright © 2020 Vaca Roxa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
